5999060ee9375b11e7647b04fef06215ed410613
AAKASH HANDA <AAKASH HANDA>
2017-02-08 06:49:52 +0530

    a

--- src/routes.js
+++ src/routes.js
@@ -1,24 +1,17 @@
-
+'use strict'
 
 var React = require('react');
-var Router =  require('react-router');
-var DefaultRoute  = Router.DefaultRoute;
-var Route = Router.Route;
-var NotFoundRoute  = Router.NotFoundRoute;
-var Redirect   = Router.Redirect;
+
+var Router = require('react-router');
+var DefaultRoute = Router.DefaultRoute;
+var Route= Router.Route;
 
 var routes =(
-	<Route name="app" path ="/" handler = {require("./components/app")}>
-	  <DefaultRoute handler= {require('./components/homePage')} />
-	  <Route name = "authors"  handler = {require("./components/authors/authorPage")} />
-	  <Route name = "about"  handler = {require("./components/about/aboutPage")} />
-	  <Route name = "addAuthor"  path="author" handler = {require("./components/authors/manageAuthorPage")} />
-	  <Route name = "manageAuthor"  path="author/:id" handler = {require("./components/authors/manageAuthorPage")} />
-	  <NotFoundRoute handler= {require('./components/notFoundPage')} />
-	   <Redirect from="about-us" to="about" />
-	   <Redirect from="about/*" to="about"  />
-	   <Redirect from="author" to="authors"  />
+	<Route name="app" path="/" handler={require('./components/app')}>
+		<DefaultRoute handler={require('./components/homePage')} />
+		<Route name="authors" handler={require('./components/authors/authorPage')}>
+		<Route name="about"  handler={require('./components/about/aboutPage')}>
 	</Route>
-	);
+)
 
-module.exports= routes;
+module.exports = routes;
